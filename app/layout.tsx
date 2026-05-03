149933502011adfa006da07b5c7a2a612456f97f
nerdylua <nihaalsprakash.is23@rvce.edu.in>
2026-05-03 11:13:20 +0530

    fix: improve clarity logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,9 @@ export default function RootLayout({
         <Script id='clarity' strategy='afterInteractive'>
           {`
             (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              c[a]=typeof c[a] === "function"
+                ? c[a]
+                : function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "wjor4iygs1");
